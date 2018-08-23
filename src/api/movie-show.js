311379acbd02875a9df91fee7e9596fb62c35a07
royalzhao <royal_zhaoshuai@163.com>
2018-08-23 17:56:07 +0800

    更新

--- src/api/movie-show.js
+++ src/api/movie-show.js
@@ -1,34 +1,37 @@
-import axios from 'axios';
+import { ajax } from '@/utils/index'
 import { commonParams } from './config';
 
-const baseURL = 'http://api.douban.com'
-
 export function getMovie(start, count) {
-  const url = `${baseURL}/v2/movie/in_theaters`;
+  const url = `/v2/movie/in_theaters`;
   const data = Object.assign({}, commonParams, {
     start,
     count
   });
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data);
-  }).catch((err) => {
-    console.log(err);
-  });
+  
+  return new Promise((resolve, reject) => {
+    ajax.get(url, data)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
 }
 
 export function getComingMovie(start, count) {
-  const url = `${baseURL}/v2/movie/coming_soon`;
+  const url = `/v2/movie/coming_soon`;
   const data = Object.assign({}, commonParams, {
     start,
     count
   });
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data);
-  }).catch((err) => {
-    console.log(err);
-  });
+  return new Promise((resolve, reject) => {
+    ajax.get(url, data)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+const baseURL = 'https://api.douban.com'
+
 function formatNumber (n) {
   const str = n.toString()
   return str[1] ? str : `0${str}`
@@ -18,7 +20,81 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+/**
+ * @desc   判断是否为手机号
+ * @param  {String|Number} str
+ * @return {Boolean}
+ */
+export const isPhoneNum = (str) => {
+  return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
+}
+
+export const showToast = (text) => {
+  wx.showToast({
+    title: text,
+    icon: 'none',
+    duration: 2000
+  })
+}
+export const showLoding = (title, content) => {
+  return new Promise((resolve, reject) => {
+    wx.showModal({
+      title,
+      content,
+      success (res) {
+        if (res.confirm) {
+          // console.log('用户点击确定')
+          resolve(res)
+        } else if (res.cancel) {
+          // console.log('用户点击取消')
+          reject(res.cancel)
+        }
+      }
+    })
+  })
+}
+export const ajax = {
+  get (url, params) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: `${baseURL}${url}`,
+        data: params,
+        method: 'GET',
+        header: {
+          "content-type":"json"
+        },
+        success (res) {
+          resolve(res)
+        },
+        fail (res) {
+          reject(res)
+        }
+      })
+    })
+  },
+  post (url, params) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: `${baseURL}${url}`,
+        data: params,
+        method: 'POST',
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success (res) {
+          resolve(res)
+        },
+        fail (res) {
+          reject(res)
+        }
+      })
+    })
+  }
+}
 export default {
   formatNumber,
-  formatTime
+  formatTime,
+  showToast,
+  ajax,
+  isPhoneNum
 }
